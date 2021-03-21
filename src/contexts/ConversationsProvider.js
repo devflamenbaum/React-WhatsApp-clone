1e119031b326e2efd07f2a4880a6259836b24edc
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-03-21 10:41:58 -0300

    Finishing Client

--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -9,7 +9,7 @@ export function useConversations(){
     return useContext(ConversationsContext)
 }
 
-export function ConversationsProvider({children}) {
+export function ConversationsProvider({id, children}) {
 
     const [conversations, setConversations] = useLocalStorage('conversations', [])
     const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
@@ -30,15 +30,56 @@ export function ConversationsProvider({children}) {
             return {id: recipient, name: name}
         })
 
+        const messages = conversation.messages.map(message => {
+            const contact = contacts.find(contact => {
+                return contact.id === message.sender
+            })
+            const name = (contact && contact.name) || message.sender
+            const fromMe = id === message.sender
+            return {...message, senderName: name, fromMe}
+        })
+
         const selected = index === selectedConversationIndex
 
-        return {...conversation, recipients, selected}
+        return {...conversation, messages, recipients, selected}
         
     })
+
+    const addMessageToConversation = ({ recipients, text, sender}) => {
+        setConversations(prevConversations => {
+            let madeChange = false
+            const newMessage = { sender, text }
+            const newConversations = prevConversations.map( conversation => {
+                if(arrayEquality(conversation.recipients, recipients)){
+                    madeChange = true
+                    return {
+                        ...conversation, 
+                        messages: [...conversation.messages, newMessage]
+                    }
+                }
+
+                return conversation
+            })
+
+            if(madeChange){
+                return newConversations
+            }else{
+                return [
+                    ...prevConversations, 
+                    { recipients, messages: [newMessage] }
+                    ]
+            }
+        })
+    }
+
+    const sendMessage = (recipients, text) => {
+        addMessageToConversation({ recipients, text, sender: id})
+    }
 
     const value = {
         conversations: formattedConversations,
         selectedConversation: formattedConversations[selectedConversationIndex],
+        sendMessage,
         selectedConversationIndex: setSelectedConversationIndex,
         createConversation
     }
@@ -48,4 +89,15 @@ export function ConversationsProvider({children}) {
             { children }
         </ConversationsContext.Provider>
     )
+}
+
+const arrayEquality = (a, b) => {
+    if(a.length !== b.length) return false
+
+    a.sort()
+    b.sort()
+
+    return a.every((element,index) => {
+        return element === b[index]
+    })
 }
